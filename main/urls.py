00c73ff314c6b63fb198d38271c40741099135fd
egor-or-love <egor.mpa@gmail.com>
2022-04-12 02:06:47 +0500

    Change user's information form added.

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import index, other_page
+from .views import BBLogoutView, ChangeUserInfoView
+from .views import index, other_page, profile
 from django.contrib.auth.views import LoginView
 
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/login/', LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,11 +3,30 @@ from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
+from .models import AdvUser
+from .forms import ChangeUserInfoForm
 
 
 # Create your views here.
+@login_required
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
 class BBLoginView(LoginView):
     template_name = 'main/login, html'
+
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
 
 
 def index(request):
@@ -20,3 +39,20 @@ def other_page(request, page):
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Данные пользователя изменены'
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
